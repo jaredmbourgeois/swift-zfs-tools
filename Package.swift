5ba032e54f99e33b5abef15be6f0dc01f91e95ea
Jared Bourgeois <mail@jaredbourgeois.com>
2022-09-10 22:35:33 -0500

    000_012 executableTarget

--- Package.swift
+++ Package.swift
@@ -4,38 +4,38 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-zfs-tools",
-    platforms: [
-      .macOS(.v12),
-      .custom("Ubuntu", versionString: "18.0.4")
-    ],
-    products: [
-        .executable(
-            name: "swift-zfs-tools",
-            targets: ["swift-zfs-tools"]
-        )
-    ],
-    dependencies: [
-        .package(
-          url: "https://github.com/jaredmbourgeois/swift-shell",
-          from: "1.0.7"
-        ),
-        .package(
-          url: "https://github.com/apple/swift-algorithms",
-          from: "1.0.0"
-        )
-    ],
-    targets: [
-        .target(
-            name: "swift-zfs-tools",
-            dependencies: [
-              .product(name: "Shell", package: "swift-shell"),
-              .product(name: "Algorithms", package: "swift-algorithms")
-            ]
-        ),
-        .testTarget(
-            name: "swift-zfs-tools-tests",
-            dependencies: ["swift-zfs-tools"]
-        )
-    ]
+  name: "swift-zfs-tools",
+  platforms: [
+    .macOS(.v12),
+    .custom("Ubuntu", versionString: "18.0.4")
+  ],
+  products: [
+    .executable(
+      name: "swift-zfs-tools",
+      targets: ["swift-zfs-tools"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/jaredmbourgeois/swift-shell",
+      from: "1.0.7"
+    ),
+    .package(
+      url: "https://github.com/apple/swift-algorithms",
+      from: "1.0.0"
+    )
+  ],
+  targets: [
+    .executableTarget(
+      name: "swift-zfs-tools",
+      dependencies: [
+        .product(name: "Shell", package: "swift-shell"),
+        .product(name: "Algorithms", package: "swift-algorithms")
+      ]
+    ),
+    .testTarget(
+        name: "swift-zfs-tools-tests",
+        dependencies: ["swift-zfs-tools"]
+    )
+  ]
 )
